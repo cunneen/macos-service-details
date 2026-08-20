b1c6d1d8fc2da33bc87370849a28e249ea8eee1e
Mike Cunneen <mike@appworkshop.net>
2026-08-21 02:28:42 +0800

    feat: :sparkles: tauri init

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,23 @@
 /// <reference types="vitest/config" />
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      ignored: ["**/src-tauri/**"],
+    },
+  },
   test: {
     coverage: {
       enabled: true,
       exclude: [
         // excluding sudoCommand from coverage, since the default
         //  test setup mocks it to avoid user interaction
-        "sudoCommand.ts"
-      ]
-    }
-  }
-})
+        "sudoCommand.ts",
+      ],
+    },
+  },
+});
